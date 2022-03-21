f2745a1257b1da4bda2e7dbd1a4ebd7954882fba
gimmeCoffeee <93482616+gimmeCoffeee@users.noreply.github.com>
2022-03-20 23:54:35 -0400

    Created a small array for Pokemon

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList = [
+    { name: 'Bulbasaur', height: '.7', types: ['grass', 'poison']};
+    { name: 'Ivysaur', height: '1', types: ['grass', 'poison']};
+    { name: 'Venusaur', height: '2', types: ['grass', 'poison']};
+    { name: 'Charmander', height: '.6', types: 'fire'};
+    { name: 'Charmeleon', height: '1.1', types: 'fire'};
+    { name: 'Charizard', height: '1.7', types: ['fire', 'flying']};
+];
