b9d5355794d0501f5e082a882b8fa568c23ce1d3
gimmeCoffeee <93482616+gimmeCoffeee@users.noreply.github.com>
2022-03-23 09:40:50 -0400

    Moved Scripts from js folder, then deleted the js folder

--- scripts.js
+++ scripts.js
@@ -1,5 +1,43 @@
-alert('Hello World');
+let pokemonList = [
+    {
+        name:  "Bulbasaur",
+        height: '.7',
+        types: ['grass', 'poison']
+    },
+    {
+        name:  "Ivysaur",
+        height: '1',
+        types: ['grass', 'poison']
+    },
+    {
+        name: "Venusaur",
+        height: '2',
+        types: ['grass', 'poison']
+    },
+    {
+        name: "Charmander",
+        height: '.6',
+        types: ['fire'],
+    },
+    {
+        name: "Charmeleon",
+        height: '1.1',
+        types: ['fire'],
+    },
+    {
+        name: "Charizard",
+        height: '1.7',
+        types: ['fire', 'flying'],
+    },
 
-let favoriteFood = 'pizza';
+];
 
-document.write(favoriteFood);
+/*
+for (let i = 0; i<pokemonList_.length; i++) {
+    console.log(pokemonList_[i].name);
+    console.log(pokemonList_[i].height);
+    for(let j = 0; j < pokemonList_[i].types.length; j++ )
+    {
+     console.log(pokemonList_[i].types[j])   
+    }
+} */
