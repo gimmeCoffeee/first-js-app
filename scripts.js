7e527c928b4d5b6dd174af148628e45e97931672
gimmeCoffeee <93482616+gimmeCoffeee@users.noreply.github.com>
2022-03-28 09:45:24 -0400

    Update scripts.js
    Wrapped Array in IIFE, called it into console and document

--- scripts.js
+++ scripts.js
@@ -1,36 +1,55 @@
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name:  "Bulbasaur",
+            height: '.7',
+            types: ['grass', ' poison']
+        },
+        {
+            name:  "Ivysaur",
+            height: '1',
+            types: ['grass', ' poison']
+        },
+        {
+            name: "Venusaur",
+            height: '2',
+            types: ['grass', ' poison']
+        },
+        {
+            name: "Charmander",
+            height: '.6',
+            types: ['fire'],
+        },
+        {
+            name: "Charmeleon",
+            height: '1.1',
+            types: ['fire'],
+        },
+        {
+            name: "Charizard",
+            height: '1.7',
+            types: ['fire', ' flying'],
+        },
+    ];
 
-let pokemonList = [
-     {
-        name:  "Bulbasaur",
-        height: '.7',
-        types: ['grass', ' poison']
-    },
-    {
-        name:  "Ivysaur",
-        height: '1',
-        types: ['grass', ' poison']
-    },
-    {
-        name: "Venusaur",
-        height: '2',
-        types: ['grass', ' poison']
-    },
-    {
-        name: "Charmander",
-        height: '.6',
-        types: ['fire'],
-    },
-    {
-        name: "Charmeleon",
-        height: '1.1',
-        types: ['fire'],
-    },
-    {
-        name: "Charizard",
-        height: '1.7',
-        types: ['fire', ' flying'],
-    },
-];
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+
+        function getAll() {
+            return pokemonList;
+        }
+
+        return {
+            add: add,
+            getAll: getAll
+        };
+
+        document.write(pokemonList);
+
+})();
+
+console.log(pokemonRepository.getAll())
 
 
 /* For Loop of pokemonList
@@ -56,7 +75,7 @@ for (let i = 0; i<pokemonList.length; i++) {
 */
 
 // forEach Loop for pokemonList
-pokemonList.forEach(function(pokemons) {
+pokemonRepository.getAll().forEach(function (pokemons) {
     console.log("Name: " + pokemons.name + " Height: " + pokemons.height + " Types: " + pokemons.types);
 
     {
